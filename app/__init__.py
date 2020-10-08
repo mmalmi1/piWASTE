c809bd7ae7ae4c19535e3f9386e02bed742d7e1c
jutava <jutava@hotmail.fi>
2020-10-08 15:54:11 +0300

    Add tables for users and product reviews

--- app/__init__.py
+++ app/__init__.py
@@ -30,18 +30,34 @@ def create_app(test_config=None):
         return 'Hello, World!'
 
     from . import db
-    @app.route('/get_all_product_ids')
-    def get_from_db():
-        command = f"SELECT id FROM product"
-        ids = db.get_from_db(command)
-        resp = ''
-        for i in ids:
-            resp += str(i['id']) + ":"
-        return resp
+    # get all entrys in a specified database
+    # prints them into the server log
+    @app.route('/get_all_entrys/<string:database>')
+    def get_from_db(database=None):
+        command = f"SELECT * FROM {database}"
+        entrys = db.get_from_db(command)
+        for i in entrys:
+            print(tuple(i))
+        return "OK"
 
-    @app.route('/push/<string:name>/<float:price>/<string:desc>')
-    def push_into_db(name=None, price=None, desc=None):
-        command = f'INSERT INTO product (name, price, description) VALUES ("{name}", "{price}", "{desc}")'
+    # create entry to products table
+    @app.route('/push_products/<string:name>/<float:price>/<string:desc>')
+    def push_product(name=None, price=None, desc=None):
+        command = f'INSERT INTO products (name, price, description) VALUES ("{name}", "{price}", "{desc}")'
+        db.push_into_db(command)
+        return "OK"
+
+    # create entry to users table
+    @app.route('/push_users/<string:username>/<string:password>/<int:access_level>')
+    def push_user(username=None, password=None, access_level=None):
+        command = f'INSERT INTO users (username, password, access_level) VALUES ("{username}", "{password}", "{access_level}")'
+        db.push_into_db(command)
+        return "OK"
+
+    # create entry to reviews table
+    @app.route('/push_reviews/<string:text>/<int:user_id>/<int:product_id>')
+    def push_review(text=None, user_id=None, product_id=None):
+        command = f'INSERT INTO reviews (text, user_id, product_id) VALUES ("{text}", "{user_id}", "{product_id}")'
         db.push_into_db(command)
         return "OK"
 
--- app/schema.sql
+++ app/schema.sql
@@ -1,10 +1,28 @@
-DROP TABLE IF EXISTS product;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS reviews;
 
-CREATE TABLE product (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE products (
+  product_id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT UNIQUE NOT NULL,
   price REAL NOT NULL,
   description TEXT NOT NULL,
   image TEXT,
   stock INTEGER DEFAULT 0
+);
+
+CREATE TABLE users (
+  user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL,
+  access_level INTEGER NOT NULL
+);
+
+CREATE TABLE reviews (
+  review_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  text TEXT UNIQUE NOT NULL,
+  user_id INTEGER NOT NULL,
+  product_id INTEGER NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (user_id),
+  FOREIGN KEY (product_id) REFERENCES products (product_id)
 );
