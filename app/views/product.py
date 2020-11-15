1e55819ee62ed1baca2814210af2ba621727482a
stola <auistola@student.oulu.fi>
2020-11-15 15:11:31 +0200

    Fixes to product page

--- app/views/product.py
+++ app/views/product.py
@@ -5,11 +5,30 @@ mod = Blueprint('product', __name__)
 
 @mod.route('/product/<int:productId>')
 def product(productId=None):
+    '''
+    Product view.
+    CREATE TABLE products (
+        product_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT UNIQUE NOT NULL,
+        price REAL NOT NULL,
+        description TEXT NOT NULL,
+        image TEXT,
+        stock INTEGER DEFAULT 0
+    );
+    '''
 
     command = f'SELECT * FROM products WHERE product_id = {productId}'
     prod = db.get_from_db(command)
-    print(tuple(prod))
-    resp = make_response(render_template('product.html'))
+    prod = prod.fetchall()[0]
+    print(prod["product_id"])
+    print(prod["name"])
+    print(prod["price"])
+    print(prod["description"])
+    print(prod["image"])
+    print(prod["stock"])
+    resp = make_response(render_template('product.html', product_id=prod["product_id"], name=prod["name"],
+                                        price=prod["price"], description=prod["description"],
+                                        image=prod["image"], stock=prod["stock"]))
     return resp
 
 @mod.route('/product/<int:productId>/addToCart')
