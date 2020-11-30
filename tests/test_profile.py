572e1c3579e0be31d03016c334861daa25953a3c
Topias Syri <tpsy132@gmail.com>
2020-11-30 13:51:35 +0200

    Round numbers in cart, fix tests

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -13,8 +13,8 @@ def test_profile_logged_out(client):
     client.set_cookie('localhost', 'logged_in', 'False')
     client.set_cookie('localhost', 'access_level', '0')
     page = client.get("/profile")
-    assert page.status_code == 401
-    assert b'Need to log in to access profile information' in page.data
+    assert page.status_code == 302
+    assert b'You should be redirected automatically to target URL' in page.data
     
 def test_profile_edit_logged_in(client):
     client.set_cookie('localhost', 'user_id', '99999')
