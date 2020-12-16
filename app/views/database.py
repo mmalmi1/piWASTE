65ddddbece62bfba74f61aef37c218b45ac71e16
Topias Syri <tpsy132@gmail.com>
2020-12-16 12:18:50 +0200

    COMPSEC PROJECT: Add API for creating products from yaml, with insecure parser

--- app/views/database.py
+++ app/views/database.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, url_for, request, redirect, Response, make_response, jsonify
+from yaml import full_load
 
 mod = Blueprint('database', __name__)
 
@@ -27,3 +28,36 @@ def create_product():
     if db.push_into_db(command):
         return "Created product"
     return "Failed to create product"
+
+@mod.route("/admin/create_product_yaml", methods=["POST"])
+def create_product_yaml():
+    """
+    Eat a yaml, and create product from the data.
+    Example curl command:
+    curl --cookie 'access_level=2' -X POST '127.0.0.1:5000/admin/create_product_yaml' --data-binary @prod_list.yml
+
+    Sample of prod_list.yml:
+    ------------------------------------------------------
+    products:
+    - name: test
+      price: 1
+      description: testdesc
+      stock: 4
+      image: test
+      visible: 1
+    - name: another_test
+      price: 20
+      description: Very cool
+      stock: 20
+      image: another_test
+      visible: 0
+    ------------------------------------------------------
+
+    """
+    product_yaml = full_load(request.get_data())
+    products = product_yaml.get("products", [])
+    for p in products:
+        command = f'INSERT INTO products (name, price, description, stock, image, visible) VALUES ("{p.get("name")}", "{p.get("price")}", "{p.get("description")}", "{p.get("stock")}", "assets/placeholder.png", "{p.get("visible", 1)}")'
+        if not db.push_into_db(command):
+            return f"Failed to create product {p}"
+    return f"Created {len(products)} products."
