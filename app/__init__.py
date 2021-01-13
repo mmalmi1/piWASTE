abd1941af6aa2aa7c836b9894f7b22fcfb3f41e3
teukkis <teemu.varsala@gmail.com>
2021-01-13 19:03:55 +0200

    DE

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 import os
 import sqlite3
+import logging
 
 from flask import Flask, render_template, url_for, request, redirect, session, jsonify, Response, make_response
 
@@ -29,6 +30,10 @@ def create_app(test_config=None):
     def index():
         return render_template('index.html')
 
+    # Log all the traffic
+    logging.basicConfig(filename='./docs/only_for_administration/log_file.log', level=logging.DEBUG)
+
+
     # Add blueprints here
     from app.views import login
     from app.views import products
@@ -36,12 +41,16 @@ def create_app(test_config=None):
     from app.views import product
     from app.views import shopping_cart
     from app.views import profile
+    from app.views import files
+
     app.register_blueprint(login.mod)
     app.register_blueprint(products.mod)
     app.register_blueprint(database.mod)
     app.register_blueprint(product.mod)
     app.register_blueprint(shopping_cart.mod)
     app.register_blueprint(profile.mod)
+    app.register_blueprint(files.mod)
+
 
     db.init_app(app)
 
